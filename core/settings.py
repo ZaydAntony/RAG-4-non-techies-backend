40fbe195998f8fe085df9384c269d6d358b87243
Zayd Antony <zaydantony123@gmail.com>
2026-06-07 22:54:29 +0300

    Clean up print statements

--- core/settings.py
+++ core/settings.py
@@ -87,7 +87,7 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 DATABASE_URL = os.getenv("DATABASE_URL")
 
-print("DATABASE_URL =", repr(DATABASE_URL))
+
 
 if DATABASE_URL:
     DATABASES = {
