066dec953d39f1b8777f2cc088b4c7d54801e972
Zayd Antony <zaydantony123@gmail.com>
2026-06-07 23:54:34 +0300

    fixed string lit

--- core/urls.py
+++ core/urls.py
@@ -21,5 +21,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/', include('travisjr.urls'))
-    path('',TemplateView.as_view(template_name='index.html))
+    path('',TemplateView.as_view(template_name='index.html'))
 ]
