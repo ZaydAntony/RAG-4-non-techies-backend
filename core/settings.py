693c4f2f1cc6e71ddeeb59f2b5ab06da1b91f7e2
Zayd Antony <zaydantony123@gmail.com>
2026-06-07 23:32:31 +0300

    fixed db urlconfusion

--- core/settings.py
+++ core/settings.py
@@ -85,12 +85,13 @@ WSGI_APPLICATION = 'core.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
-SUPABASE_URL = os.getenv("SUPABASE_URL")
+DATABASE_URL = os.getenv("DATABASE_URL")
 
-if SUPABASE_URL:
+
+if DATABASE_URL:
     DATABASES = {
         "default": dj_database_url.parse(
-            SUPABASE_URL,
+            DATABASE_URL,
             conn_max_age=600,
             ssl_require=True,
         )
